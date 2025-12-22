c9df1d96e9ff2ba49c23c9757cc47435ab7f4a7f
jhansi-challa <jhansich235@gmail.com>
2025-12-22 15:35:45 +0530

    Time: 3 ms (83.45%), Space: 46.8 MB (21.95%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String reverseVowels(String s) {
+        Set<Character> vowels = new HashSet<>();
+        for (char c : "aeiouAEIOU".toCharArray()) {
+            vowels.add(c);
+        }
+
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            while (left < right && !vowels.contains(chars[left])) {
+                left++;
+            }
+            while (left < right && !vowels.contains(chars[right])) {
+                right--;
+            }
+
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.reverseVowels("IceCreAm"));
+        System.out.println(sol.reverseVowels("leetcode"));
+    }
+}
