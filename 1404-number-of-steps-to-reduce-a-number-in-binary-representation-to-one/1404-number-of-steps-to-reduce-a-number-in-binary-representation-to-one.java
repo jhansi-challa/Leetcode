624447e9e64d8fdfeda97d07b3ba9071c9329ad7
jhansi-challa <jhansich235@gmail.com>
2026-03-05 10:48:16 +0530

    Time: 1 ms (36.07%), Space: 42.7 MB (68.33%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,22 +1,37 @@
-import java.math.BigInteger;
-
 public class Solution {
     public int numSteps(String s) {
-        BigInteger num = new BigInteger(s, 2); // convert binary string to BigInteger
+        // Convert string to char array for easier manipulation
+        char[] bin = s.toCharArray();
         int steps = 0;
-        BigInteger TWO = BigInteger.valueOf(2);
+        int n = bin.length;
 
-        while (num.compareTo(BigInteger.ONE) > 0) {
-            if (num.mod(TWO).equals(BigInteger.ZERO)) {
-                // even → divide by 2
-                num = num.divide(TWO);
+        // Start from the least significant bit (rightmost)
+        int i = n - 1;
+        while (i > 0) { // stop when only one bit remains (the '1' at the front)
+            if (bin[i] == '0') {
+                // even → divide by 2: just move to the next bit
+                i--;
             } else {
                 // odd → add 1
-                num = num.add(BigInteger.ONE);
+                bin[i] = '0'; // flipping current bit
+                int j = i - 1;
+                while (j >= 0 && bin[j] == '1') {
+                    bin[j] = '0';
+                    j--;
+                }
+                if (j >= 0) {
+                    bin[j] = '1';
+                } else {
+                    // carry reached the front → need to add a new '1' at the start
+                    char[] newBin = new char[bin.length + 1];
+                    newBin[0] = '1';
+                    System.arraycopy(bin, 0, newBin, 1, bin.length);
+                    bin = newBin;
+                    i++; // adjust index due to new length
+                }
             }
             steps++;
         }
-
         return steps;
     }
 }
